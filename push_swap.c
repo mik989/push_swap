40da5c59f1e9bbcab03ef6943d041fef6495b76a
Michele <gir89@live.it>
2022-12-04 21:16:01 +0100

    check casi limite

---
+++ push_swap.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include "lib/libft.h"
+
+int main (int ac, char **av)
+{
+    int j;
+    int i;
+
+    j = 1;
+        if(ac == 1)
+            return 0;
+         
+        while(j < ac)
+        {
+            while(av[j][i] != '\0' && (j < ac - 1))
+                {
+                    if (ft_isdigit(av[j][i]))
+                        i++;
+                    else
+                    {   write(2, "error\n", 6);
+                        exit(0);
+                    }
+                }
+                j++;
+        }
+        j = 1;
+        i = j + 1;
+        while(j < ac)
+        {
+            while(av[j] && (i < ac))
+                {
+                    if(ft_atoi(av[j]) == ft_atoi(av[i]))
+                    {
+                        write(2, "error\n", 6);
+                        exit(0);
+                    }
+                    else
+                        i++;
+                }
+            j++;
+        }        
+        
+        if (ac == 2)
+            return 0;
+        /*if(ac > 2)
+        {
+
+        }*/
+}
